a31dfe6884cdeebcd4b1cf32322a7de1b52a6d10
reza <rezabudget@gmail.com>
2024-05-20 21:19:50 +0700

    first commit

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : M.Reza.F')
+st.subheader('NIM : 312210156')
+st.subheader('Kelas : TI.22.B1')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/amar1.jpeg", caption="M.Reza.F", use_column_width="Always", width=200)
+
+
+st.link_button("Instagram", "https://www.instagram.com/muammar212/")
+st.link_button("Github", "https://github.com/muammarM")
